78a00d92cda9adbe563c49f534bb8ad52091ef65
calebmutisya <mutisyacalebmusau@gmail.com>
2023-10-11 12:35:38 +0300

    Can add car to list using sell a car from

--- script.js
+++ script.js
@@ -21,13 +21,81 @@ function displayVehicles(vehicles){
               <p id="cardMileage" class="card-text">${vehicle.mileage}</p>
               Engine Type:
               <p id="cardEngine" class="card-text">${vehicle.engine}</p>
-              <button class="button">See More</button>
+              <button onclick="displayDetails(${vehicle.id})" class="button-green">See More</button>
             </div>
           </div>
         `
         carList.appendChild(singleVehicle); 
-        
-        
-
     });
 }
+//Add Car to List of Vehicles
+document.addEventListener('DOMContentLoaded',()=>{
+    const carForm= document.getElementById('new-car');
+    carForm.addEventListener('submit',(event)=>{
+        event.preventDefault();
+
+        const Model= document.getElementById('newModel').value;
+        const Price= document.getElementById('newPrice').value;
+        const Engine= document.getElementById('newEngine').value;
+        const cc= document.getElementById('newCC').value;
+        const Mileage= document.getElementById('newMileage').value;
+        const Image= document.getElementById('newImage').value;
+        const Description= document.getElementById('newDescription').value;
+
+        const newCar={
+            model: Model,
+            price: Price,
+            engine: Engine,
+            cc: cc,
+            mileage: Mileage,
+            image: Image,
+            description: Description,
+        };
+
+        fetch(`http://localhost:3000/vehicles`,{
+            method:'POST',
+            body: JSON.stringify(newCar),
+            headers:{
+                'Content-Type':'application/json',
+            },
+            
+        })
+        .then(response=> response.json())
+        .then((data)=>(alert('Car On Sale')));
+    });
+})
+
+//FetchDisplayDetails
+function displayDetails(vehiclesId){
+    return fetch(`http://localhost:3000/vehicles/${vehiclesId}`)
+        .then(response=> response.json())
+        .then(data => {displayVehicleDetails(data);
+        })
+}
+//DisplayVehicleDetails
+function displayVehicleDetails(vehicle){
+    const detailsSection= document.getElementByClass('car-specs');
+    detailsSection.innerHTML=`
+    <img class="main-img" src="${vehicle.image}" alt="Loading ..">
+          <div class="details-section">
+            <h5>${vehicle.model}</h5>
+            <div class="main-details ">
+              <p>${vehicle.price}</p>
+              <p>${vehicle.mileage}</p>
+              <p>${vehicle.engine}</p>
+              <p>${vehicle.cc}</p>
+            </div  >
+            <div>
+              <button class="button-green">BID</button>
+              <button class="button-green">Edit</button>
+              <button class="button-red">Delete</button>
+            </div>
+            <h6>Description :</h6>
+            <p class="main-description">${vehicle.description}</p>
+
+          </div>
+    `
+
+
+}
+
